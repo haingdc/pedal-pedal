515b28d9f8131aa9559e5fe8a31558b93e33d849
Hai.Nguyen <haingdc@gmail.com>
2025-08-05 11:32:31 +0700

    thêm roadmap từ link figma

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -4,6 +4,10 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Dự án' },
   { href: '/about', title: 'Giới thiệu' },
+  {
+    href: 'https://www.figma.com/board/CIo4vLPW0BevdY27kljGhO/Hai-Nguyen-s-Public-Roadmap?node-id=0-1&t=3TIdYprjTWjYV9Ar-1',
+    title: 'Road map',
+  },
 ]
 
 export default headerNavLinks
