8033147dc2202f29350a0fe274adda21c02f01e5
Hai.Nguyen <haingdc@gmail.com>
2025-09-28 20:05:35 +0700

    cập nhật property cho 2 icon component

--- components/icons/Pen.tsx
+++ components/icons/Pen.tsx
@@ -8,9 +8,9 @@ export default function Pen(svgProps: SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...svgProps}
     >
       <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
